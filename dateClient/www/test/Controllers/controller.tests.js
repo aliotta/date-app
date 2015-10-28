fe4b685a335fb316324372ec69f39f88ac5104d9
Alex Liotta <aliotta343@gmail.com>
2015-10-27 18:45:59 -0700

    (fix) CLient side tests work again

--- dateClient/www/test/Controllers/controller.tests.js
+++ dateClient/www/test/Controllers/controller.tests.js
@@ -128,6 +128,7 @@ describe('FindADateCtrl', function(){
     location = _$location_;
     $controller('FindADateCtrl', {$scope: scope, $stateParams: stateParams, $cordovaGeolocation: cordovaGeolocation, $ionicHistory: ionicHistory, $ionicPlatform: ionicPlatform, $location: location});
 
+
   }));
 
   it('should have obj currentQuestion equal to the current question', function(){
