5d557c82a582195a724fb1cd407d0db70d835b43
jessicalc <jessica@crayoncollective.com>
2015-10-28 15:56:06 -0700

    (feat) Added categories and opening hours to idea view
    Also added some CSS styles

--- dateClient/www/js/controllers/ideas.js
+++ dateClient/www/js/controllers/ideas.js
@@ -2,6 +2,7 @@ angular.module('dateworthy.ideas', [])
 .controller('IdeasCtrl', function($scope, $timeout, $location, DateData, LikeADate) {
 
   $scope.ideas = DateData.getDateIdeas();
+
   // Wrapping the `DateData.getDateIdeas` function call inside a $scope.on('$stateChangeSuccess') listener.
   // $stateChangeSuccess - fired once the state transition is complete (https://github.com/angular-ui/ui-router/wiki)
   $scope.$on('$stateChangeSuccess', function() {
