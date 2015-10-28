9f318da14b4932b37fc00c3e09a3d2af2d6590e7
Alex Liotta <aliotta343@gmail.com>
2015-10-28 15:02:41 -0700

    (feat) Continuous integration

--- dateServer/test/models/event.js
+++ dateServer/test/models/event.js
@@ -115,25 +115,25 @@ function expectEventnameTakenValidationError(err, eventname) {
 describe('Event models:', function () {
 
     
-    // before(function (done) {
-    //    db.createConstraint({
-    //        label: 'Event',
-    //        property: 'eventname',
-    //    }, function (err, constraint) {
-    //        if (err) throw err;     // Failing fast for now, by crash the application.
-    //        if (constraint) {
-    //             console.log('(Registered unique eventnames constraint.)');
-    //        } else {
-    //            // Constraint already present; no need to log anything.
-    //        }
-    //        done();
-    //    })
-    // })
+    before(function (done) {
+       db.createConstraint({
+           label: 'Event',
+           property: 'eventname',
+       }, function (err, constraint) {
+           if (err) throw err;     // Failing fast for now, by crash the application.
+           if (constraint) {
+                console.log('(Registered unique eventnames constraint.)');
+           } else {
+               // Constraint already present; no need to log anything.
+           }
+           done();
+       })
+    })
     
 
     it('List initial events', function (next) {
         this.timeout = 5000
-        this.setTimeout(next, 5000);
+        setTimeout(next, 5000);
         Event.getAll(function (err, events) {
             console.log(err, "ERROR")
             if (err) return next(err);
